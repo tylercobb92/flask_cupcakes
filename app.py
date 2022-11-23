c28540f0754b1cf9436d902dce7c483bc6521554
tyler <tylercobb92@gmail.com>
2022-11-23 16:58:24 -0500

    routes for root, list_cupcakes, get_cupcake, create_cupcake

--- app.py
+++ app.py
@@ -12,3 +12,32 @@ with app.app_context():
 
     connect_db(app)
 
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route('/api/cupcakes', methods=['POST'])
+def create_cupcake():
+    data = request.json
+
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        size=data['size'],
+        rating=data['rating'],
+        image=data['image' or None]
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return(jsonify(cupcake=cupcake.to_dict()), 201)
